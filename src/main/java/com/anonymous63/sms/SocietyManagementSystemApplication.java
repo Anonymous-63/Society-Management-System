205e0b23883d20dc59b4ab312086b7e15332aaa7
anonymous63 <anonymous63.krutik@gmail.com>
2025-03-10 22:29:02 +0530

    first commit

---
+++ src/main/java/com/anonymous63/sms/SocietyManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.anonymous63.sms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocietyManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocietyManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/anonymous63/sms/SocietyManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anonymous63.sms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocietyManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
